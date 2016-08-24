b1cd7e7fd1974ebeef6ca02db6fb044229724144
nunesGabriel <nunes.gabriel.13@gmail.com>
2016-08-24 16:36:43 -0300

    Initial commit

---
+++ app/src/androidTest/java/br/com/aulateste1e2/codetcc/ApplicationTest.java
@@ -0,0 +1,13 @@
+package br.com.aulateste1e2.codetcc;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
